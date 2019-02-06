32d46ed3f00f87a4a365b3bfc7930be0f7c4002f
marcushoile <marcus.hoile@gmail.com>
2019-02-06 20:10:54 +1100

    relax version control

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExbufPlug.Mixfile do
     [
       app: :exbuf_plug,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.2",
       description: "A small plug to handle decoding protocol buffers.",
       package: package(),
       build_embedded: Mix.env() == :prod,
